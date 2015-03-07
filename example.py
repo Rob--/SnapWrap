8a1c03209a7d416e77704862717ea745a525275c
Robert <valentyne.robert@gmail.com>
2015-03-07 22:25:56 +0000

    .

--- example.py
+++ example.py
@@ -1,11 +1,12 @@
-from bot import SnapchatBot
+from snapchat import Snapchat
 from requests.exceptions import HTTPError, ConnectionError
 import logging
 import time
+import utils
 
-class CustomBot(SnapchatBot):
+class CustomBot(Snapchat):
     def on_snap(self, sender, snap):
-        self.post_story(snap)
+        utils.save_snap(snap, "A:\Python\Projects\SnapWrap\SnapWrap")
         
     def on_friend_add(self, friend):
         self.add_friend(friend)
@@ -18,8 +19,8 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 while True:
     try:
         try:
-            bot = CustomBot(*["ghcollege", "garthhillcollege1"])
-            bot.listen()
+            bot = CustomBot(*["user", "pass"])
+            bot.begin()
         except (HTTPError, ConnectionError) as e:
             time.sleep(180)
     except Exception:
